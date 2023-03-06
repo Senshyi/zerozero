6569ff14727cc284c4b369200c6ba688c0a57f5a
theBeast <janmachacek97@gmail.com>
2023-03-07 00:21:45 +0100

    serer setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn greet(request: HttpRequest) -> impl Responder {
+    let name = request.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
