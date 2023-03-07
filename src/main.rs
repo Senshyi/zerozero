ff6b3da0ea2ebcdd8d68fb021314c984ee2248e3
theBeast <janmachacek97@gmail.com>
2023-03-07 21:54:23 +0100

    create integration test setup, add health check

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,6 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
-async fn greet(request: HttpRequest) -> impl Responder {
-    let name = request.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
+use zerozero::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    run()?.await
 }
