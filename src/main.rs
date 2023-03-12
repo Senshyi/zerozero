eaa53b25d3dff5c43dae7ecc05568719a0f6d76f
theBeast <janmachacek97@gmail.com>
2023-03-12 22:45:37 +0100

    setup tracing

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 use zerozero::configuration::get_configuration;
 use zerozero::startup::run;
-use zerozero::telemetry::{init_subscriber,get_subscriber};
+use zerozero::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let subscriber = get_subscriber("zerozero".into(), "info".into());
+    let subscriber = get_subscriber("zerozero".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connect_string())
-        .await
-        .expect("Failed to connect to postgres");
+    let connection_pool =
+        PgPool::connect(&configuration.database.connect_string().expose_secret())
+            .await
+            .expect("Failed to connect to postgres");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
 
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,8 +1,24 @@
+use once_cell::sync::Lazy;
+use secrecy::ExposeSecret;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
 use zerozero::configuration::{get_configuration, DatabaseSettings};
 use zerozero::startup::run;
+use zerozero::telemetry::{get_subscriber, init_subscriber};
+
+// ensure that the subscriber is initialized only once
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".into();
+    let subscriber_name = "test".into();
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
+});
 
 struct TestApp {
     pub address: String,
@@ -10,6 +26,8 @@ struct TestApp {
 }
 
 async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
@@ -28,7 +46,7 @@ async fn spawn_app() -> TestApp {
 }
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
-    let mut connection = PgConnection::connect(&config.connect_string_without_db())
+    let mut connection = PgConnection::connect(&config.connect_string_without_db().expose_secret())
         .await
         .expect("Failed to connect to db");
     connection
@@ -36,7 +54,7 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
         .await
         .expect("Failed to create database");
 
-    let connection_pool = PgPool::connect(&config.connect_string())
+    let connection_pool = PgPool::connect(&config.connect_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
 
