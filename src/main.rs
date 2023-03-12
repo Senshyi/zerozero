25f29fbc3f83e5dd390fc9b015e08ea7782fad98
theBeast <janmachacek97@gmail.com>
2023-03-12 14:57:42 +0100

    move telemetry into separate file

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,14 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
-use tracing_log::LogTracer;
 
 use zerozero::configuration::get_configuration;
 use zerozero::startup::run;
+use zerozero::telemetry::{init_subscriber,get_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    LogTracer::init().expect("Failed to set logger");
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("zerozero".into(), std::io::stdout);
-
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = get_subscriber("zerozero".into(), "info".into());
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connect_string())
